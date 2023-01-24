f43b538b711378602bab90b748984d9df046da24
RoaViviana <108878759+RoaViviana@users.noreply.github.com>
2023-01-24 00:27:43 -0300

    Mejorando código de contacto.

--- src/main/java/com/portfolio/vivi/controladores/ContactoController.java
+++ src/main/java/com/portfolio/vivi/controladores/ContactoController.java
@@ -1,7 +1,6 @@
 package com.portfolio.vivi.controladores;
 
 import com.portfolio.vivi.modelo.Contacto;
-import com.portfolio.vivi.modelo.Cursos;
 import com.portfolio.vivi.servicios.ContactoService;
 import com.portfolio.vivi.servicios.CursosService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,26 +16,5 @@ public class ContactoController {
     @PostMapping("/")
     public ResponseEntity<Contacto> guardarContacto(@RequestBody Contacto contacto){
         return ResponseEntity.ok(contactoService.agregarContacto(contacto));
-    }
-    @CrossOrigin("*")
-    @PutMapping("/")
-    public ResponseEntity<Contacto> actualizarContacto(@RequestBody Contacto contacto){
-        return ResponseEntity.ok(contactoService.actualizarContacto(contacto));
-    }
-
-    @CrossOrigin("*")
-    @GetMapping("/")
-    public ResponseEntity<?> listarContactos(){
-        return ResponseEntity.ok(contactoService.obtenerContactos());
-    }
-    @CrossOrigin("*")
-    @GetMapping("/{contactoId}")
-    public Contacto listarContacto(@PathVariable("contactoId") Long contactoId){
-        return contactoService.obtenerContacto(contactoId);
-    }
-    @CrossOrigin("*")
-    @DeleteMapping("/{contactoId}")
-    public void eliminarContacto(@PathVariable("contactoId") Long contactoId){
-        contactoService.eliminarContacto(contactoId);
     }
 }
